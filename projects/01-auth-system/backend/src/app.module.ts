3ba93770e8e72836a94374321423323f9297fede
jerome <couchy.jerome@gmail.com>
2025-12-24 00:33:34 +0100

    feat(init): init prisma + empty modules

--- projects/01-auth-system/backend/src/app.module.ts
+++ projects/01-auth-system/backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { SecurityModule } from './modules/security/security.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule, PrismaModule, SecurityModule],
   controllers: [AppController],
   providers: [AppService],
 })
